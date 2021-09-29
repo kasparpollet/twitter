1018497fbf8722f7cc6364f1dbdeadf122f7b498
Oscar Werensteijn <oscar.werensteijn@gmail.com>
2021-09-29 14:15:47 +0200

    Oscar edit
    Editing some stuff of my own

---
+++ Oscar/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+from twitter import TwitterApi
+from database import DataBase
+from unhcr import Unhcr
+
+if __name__ == "__main__":
+    # RUN CODE HERE
+    load_dotenv()
+    twitter = TwitterApi()
+    #tweet = twitter.get_id(457)
+    #hashtag = twitter.get_hashtag()
+    print(twitter.get_hashtag())
+
