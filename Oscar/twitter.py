e64c27d989a608a71422672302279296c3571167
Oscar Werensteijn <oscar.werensteijn@gmail.com>
2021-09-29 13:55:50 +0200

    Twitter api
    Gets all the tweet information from 1 hashtag and puts it in a dataframe (json format)

--- Oscar/twitter.py
+++ Oscar/twitter.py
@@ -1,6 +1,11 @@
 import requests
 import os
 import tweepy
+import pandas as pd
+import json
+import datetime
+import csv
+import time
 
 
 class TwitterApi:
@@ -10,12 +15,25 @@ class TwitterApi:
         self.token = os.getenv('TWITTER_BEARER_TOKEN')
 
     def get_hashtag(self):
-        hashtags = []
-        headers = {
-            'Authorization': f'Bearer {self.token}',
-        }
-        api = requests.get(self.base_url + "search/recent?query=%40Taliban", headers=headers).text
-        df = 
+        # Keys and access for Twitter
+        consumer_key = '0JaPiWrKpeuN5XKrJ62QjhnOZ'
+        consumer_secret = '6CfnfH8y7PQUvz1eDKTZAjOf6WCgFYFbhriMQtecBROHyOVMFr'
+        access_token = '1438084578984476677-xH18IauzOG0abRlBDIu2ilPBxSV0y9'
+        access_token_secret = '7oasPw5UUlsIOArbDHGvoZASefAs5wF4LI5wN8Lur9Iml'
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        datesince = '2020-01-01'
+        MAX_TWEETS = 10
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        api = tweepy.API(auth)
+        tweets = tweepy.Cursor(api.search_tweets, q='#Taliban', lang='en',
+                               since=datesince).items(MAX_TWEETS)
+        data = pd.DataFrame(data=[tweet for tweet in tweets], columns=['Tweets'])
+        print(data)
+        data.to_csv('tweets.csv')
+
+
         return
 
     def get_id(self, id):
