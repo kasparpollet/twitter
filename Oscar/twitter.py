77160624d28c1841a626a60d5b183cbee8197a32
Oscar Werensteijn <oscar.werensteijn@gmail.com>
2021-09-29 10:07:06 +0200

    .

--- Oscar/twitter.py
+++ Oscar/twitter.py
@@ -2,7 +2,6 @@ import requests
 import os
 import tweepy
 
-#hashtag = '#Afghanistan'
 
 class TwitterApi:
     
@@ -11,6 +10,7 @@ class TwitterApi:
         self.token = os.getenv('TWITTER_BEARER_TOKEN')
 
     def get_hashtag(self):
+        hashtags = []
         headers = {
             'Authorization': f'Bearer {self.token}',
         }
@@ -24,17 +24,3 @@ class TwitterApi:
 
         return requests.get(self.base_url + str(id), headers=headers).text
 
-    def create_url(keyword, start_date, end_date, max_results=10):
-        search_url = "https://api.twitter.com/2/tweets/search/tweets.json?q=#Afghanistan"  # Change to the endpoint you want to collect data from
-
-        # change params based on the endpoint you are using
-        query_params = {'query': keyword,
-                        'start_time': start_date,
-                        'end_time': end_date,
-                        'max_results': max_results,
-                        'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
-                        'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
-                        'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
-                        'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
-                        'next_token': {}}
-        return (search_url, query_params)
