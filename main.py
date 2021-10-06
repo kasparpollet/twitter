11e8816060bf4ebd5443bb8d1d0ceeb85e704816
Wouter Paas <wouter@MacBook-Pro-van-Wouter.local>
2021-10-06 13:37:06 +0200

    tmp push, data cleaning, stop words

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from os import remove
 from dotenv import load_dotenv
 
 from scripts.twitter import TwitterApi
 from scripts.database import DataBase
 from scripts.unhcr import Unhcr
+
+#Natural language processing tool-kit
+import nltk           
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 def get_hashtags_from_file():
     with open('./files/hashtags.txt') as f:
@@ -18,11 +24,19 @@ def clean_text(text):
     return ''.join(filter(whitelist.__contains__, cleanText))
 
 
-def remove_stopwords(text):
-    with open('./files/stopwords.txt') as f:
-        for i in f.items():
-            text = text.replace(i, '')
-    return text
+def remove_stopwords(tweet):
+    # Create stopword list
+    nltk.download("stopwords")
+    stop = set(stopwords.words('english'))
+    temp =[]
+    snow = nltk.stem.SnowballStemmer('english')
+    for index, row in tweet.iterrows():
+        print(tweet['text'])
+        words = [snow.stem(word) for word in row['text'].split() if word not in stop]
+        temp.append(words)
+        tweet.at[index, 'text'] = words
+        print(tweet['text'])
+    return temp
 
 
 def __init__():
@@ -40,3 +54,8 @@ if __name__ == "__main__":
     # db.upload_data(tweets_df, 'tweets', 'replace')
 
     #print(db.get_tweets()['text'].head(2))
+    newestId = db.get_new_id()
+    oldestId = db.get_old_id()
+    tweet = db.get_tweets()
+    #print(tweet)
+    print(remove_stopwords(tweet))
